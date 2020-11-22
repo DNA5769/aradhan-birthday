1c605ea6592c0b71064c5f8dee51d33c32cbe848
DNA5769 <dennisthomas2002@gmail.com>
2020-11-22 19:57:47 +0300

    Added ejs and md5 code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const md5 = require('md5');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
