c6171305cd5cbdeb8df6299cf3b5fa0d64d5d2c8
DNA5769 <dennisthomas2002@gmail.com>
2020-11-22 19:54:08 +0300

    Added bodyparser code

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
