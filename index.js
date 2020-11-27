d1116d57a212e90fce725a74ae091dd557ce62b0
DNA5769 <dennisthomas2002@gmail.com>
2020-11-27 18:41:39 +0300

    Added elitewishes.ejs and recquired code

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.get(`/`, (req, res) => {
+  res.render('elitewishes.ejs');
+});
+
 app.get(`/${md5('wishes')}`, (req, res) => {
   res.render('wishes.ejs');
 });
