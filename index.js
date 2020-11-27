4de08beacc737024246318da3b5d8a3369d5512c
DNA5769 <dennisthomas2002@gmail.com>
2020-11-27 18:39:08 +0300

    Routed wishes to correct path

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get(`/${md5('wishes')}`, (req, res) => {
   res.render('wishes.ejs');
 });
 
 app.listen(PORT, () => {
   console.log(`[STARTING] http://localhost:${PORT}`);
+  console.log(`[STARTING] http://localhost:${PORT}/${md5('wishes')}`);
 });
