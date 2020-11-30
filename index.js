4194a80e9dd88121ccdef8464a7b69c24559c5cb
DNA5769 <dennisthomas2002@gmail.com>
2020-11-30 14:46:07 +0300

    Basic setup of wishespage.ejs

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get(`/`, (req, res) => {
-  res.render('elitewishes.ejs');
+  res.render('wishespage.ejs');
 });
 
 app.get(`/${md5('wishes')}`, (req, res) => {
   res.render('wishes.ejs');
 });
 
+app.get(`/${md5('elitewishes')}`, (req, res) => {
+  res.render('elitewishes.ejs');
+});
+
 app.listen(PORT, () => {
   console.log(`[STARTING] http://localhost:${PORT}`);
   console.log(`[STARTING] http://localhost:${PORT}/${md5('wishes')}`);
+  console.log(`[STARTING] http://localhost:${PORT}/${md5('elitewishes')}`);
 });
